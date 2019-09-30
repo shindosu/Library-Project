91f4850911ac13d75d925e29e0de1bf9d549313d
Taisei Yamada <taiseiyamadashindosu@gmail.com>
2019-09-30 23:11:45 +0900

    editing the hamburger menu

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -2,4 +2,9 @@ $(document).ready(function(){
     $('#search').click(function(){
         $('#searchInput').fadeToggle(200);
     })
+
+    $('#toggleButton').click(function(e){
+        e.preventDefault();
+        $('#navbarSupportedContent').toggle();
+    })
 });
